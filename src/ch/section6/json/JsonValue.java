c4c2b7575ded619032d6ca4db3bef4563c84aa0b
Martin Roth <mhroth@gmail.com>
2013-08-22 02:04:15 +0200

    Formatting changes in JsonValue.
    Implements Cloneable interface.

--- src/ch/section6/json/JsonValue.java
+++ src/ch/section6/json/JsonValue.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, Martin Roth (mhroth@section6.ch)
+ * Copyright (c) 2012,2013, Martin Roth (mhroth@section6.ch)
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -41,195 +41,238 @@ import java.util.Date;
 import java.util.List;
 
 /** An abstract superclass of all JSON values. */
-public abstract class JsonValue {
-  
-  /** A enumeration of all JSON data types. */
-  public enum Type {
-    BOOLEAN,
-    NUMBER,
-    STRING,
-    DATE,
-    ARRAY,
-    MAP,
-    NULL
-  }
-  
-  /** A {@link JsonNull} singleton. */
-  protected static final JsonNull JSON_NULL = new JsonNull();
-  
-  protected static final JsonBoolean JSON_TRUE = new JsonBoolean(true);
-  
-  protected static final JsonBoolean JSON_FALSE = new JsonBoolean(false);
+public abstract class JsonValue implements Cloneable {
 
-  public String toString(int indent) {
-    if (indent < 0) throw new IllegalArgumentException();
-    
-    // if there is no indent, use the faster toString() method
-    if (indent == 0) return toString();
-    
-    List<String> tokenList = new ArrayList<String>();
-    appendTokenList(tokenList);
-    int currentIndent = 0;
-    String currentIndentString = "";
-    
-    StringBuilder sb = new StringBuilder();
-    final int len = tokenList.size();
-    for (int i = 0; i < len; ++i) {
-      String token = tokenList.get(i);
-      switch (token.charAt(0)) {
-        case '[': case '{': {
-          sb.append(token);
-          currentIndent += indent;
-          currentIndentString = spaces(currentIndent);
-          break;
-        }
-        case ']': case '}': {
-          currentIndent -= indent;
-          currentIndentString = spaces(currentIndent);
-          if (!(tokenList.get(i-1).equals("{") || tokenList.get(i-1).equals("[")))
-              sb.append(currentIndentString);
-          sb.append(token);
-          break;
-        }
-        case ':': sb.append(": "); break;
-        case '\t': sb.append(currentIndentString); break;
-        default: sb.append(token); break;
-      }
-    }
-    
-    return sb.toString();
-  }
-  
-  /** Appends all string tokens for this object to the given list. */
-  protected abstract void appendTokenList(List<String> tokenList);
-  
-  /** Returns a <code>String</code> with the given number of spaces. */
-  private String spaces(int x) {
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < x; i++) sb.append(" ");
-    return sb.toString();
-  }
-  
-  /** Returns a static reference to JSON <code>null</code>. */
-  public static JsonNull getNull() {
-  	return JSON_NULL;
-  }
-  
-  /** Returns a static reference to JSON <code>true</code> or <code>false</code>. */
-  public static JsonBoolean getBoolean(boolean b) {
-  	return b ? JSON_TRUE : JSON_FALSE;
-  }
-  
-  /** Returns a deep copy of this value. */
-  public JsonValue copy() {
-    return this;
-  }
-  
-  /** Returns the JSON type of this value. */
-  public abstract Type getType();
-
-  /** Returns this value as a {@link JsonObject}. */
-  public JsonObject asMap() throws JsonCastException {
-    throw new JsonCastException(String.format("JsonValue %s cannot be cast to a JsonObject.", toString()));
-  }
-  
-  /** Returns this value as a {@link JsonArray}. */
-  public JsonArray asArray() throws JsonCastException {
-  	throw new JsonCastException(String.format("JsonValue %s cannot be cast to a JsonArray.", toString()));
-  }
-
-  /** Returns this value as a {@link Number}. */
-  public Number asNumber() throws JsonCastException {
-  	throw new JsonCastException(String.format("JsonValue %s cannot be cast to a Number.", toString()));
-  }
-  
-  /** Returns this value as a {@link String}. */
-  public String asString() throws JsonCastException {
-  	throw new JsonCastException(String.format("JsonValue %s cannot be cast to a string.", toString()));
-  }
-  
-  /** Returns this value as a <code>boolean</code>. */
-  public boolean asBoolean() throws JsonCastException {
-  	throw new JsonCastException(String.format("JsonValue %s cannot be cast to a boolean.", toString()));
-  }
-  
-  /** Returns this value as a <code>boolean</code>. */
-  public Date asDate() throws JsonCastException {
-  	throw new JsonCastException(String.format("JsonValue %s cannot be cast to a date.", toString()));
-  }
-
-  /**
-   * A convenience method to Save this value to file with the given indent, assuming a UTF-8 character set. 
-   * @throws IOException  If something goes wrong!
-   */
-  public void saveToFile(File file, int indent) throws IOException {
-  	saveToFile(file, indent, "UTF-8");
-  }
-  
-  /**
-   * A convenience method to save this value to file with the given indent and the character set name. 
-   * @throws IOException  If something goes wrong!
-   */
-  public void saveToFile(File file, int indent, String charsetName) throws IOException {
-  	BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file));
-  	String jsonString = this.toString(indent);
-  	byte[] data = jsonString.getBytes(charsetName);
-  	out.write(data);
-  	out.flush();
-  	out.close();
-  }
-
-  /**
-   * A convenience method to load a value from file assuming a UTF-8 character set.
-   * @throws FileNotFoundException  If the file could not be found.
-   * @throws IOException  If the file could not be read.
-   * @throws JsonParseException  If the file could not be parsed into valid JSON.
-   */
-  public static JsonValue loadFromFile(File file) throws IOException {
-  	return loadFromFile(file, "UTF-8");
-  }
-  
-  /**
-   * A convenience method to load a value from file with the given character set name.
-   * @throws FileNotFoundException  If the file could not be found.
-   * @throws IOException  If the file could not be read.
-   * @throws JsonParseException  If the file could not be parsed into valid JSON.
-   */
-	public static JsonValue loadFromFile(File file, String charsetName)
-			throws FileNotFoundException, IOException, JsonParseException {
-		return loadFromStream(
-				new BufferedInputStream(new FileInputStream(file)),
-				charsetName);
+	/** A enumeration of all JSON data types. */
+	public enum Type {
+		BOOLEAN, NUMBER, STRING, DATE, ARRAY, MAP, NULL
 	}
 
-  /**
-   * A convenience method to load a value from an input stream assuming a UTF-8 character set.
-   * @throws IOException  If the input stream could not be read.
-   * @throws JsonParseException  If the file could not be parsed into valid JSON.
-   */
-  public static JsonValue loadFromStream(InputStream in)
-  		throws IOException, JsonParseException {
-  	return loadFromStream(in, "UTF-8");
-  }
-  
-  /**
-   * A convenience method to load a value from an input stream with the given character set name.
-   * @throws IOException  If the input stream could not be read.
-   * @throws JsonParseException  If the file could not be parsed into valid JSON.
-   */
-  public static JsonValue loadFromStream(InputStream in, String charsetName)
-  		throws IOException, JsonParseException {
-  	final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-  	byte[] buffer = new byte[1024]; // a 1KB buffer
-  	
-  	int len = in.read(buffer);
-  	while (len >= 0) {
-  		baos.write(buffer, 0, len);
-  		len = in.read(buffer);
-  	}
+	/** A {@link JsonNull} singleton. */
+	private static final JsonNull JSON_NULL = new JsonNull();
 
-  	return JsonObject.parse(
-  			new String(baos.toByteArray(), charsetName));
-  }
-  
+	/** A <code>true</code> <code>JsonBoolean</code> singleton. */
+	private static final JsonBoolean JSON_TRUE = new JsonBoolean(true);
+
+	/** A <code>false</code> <code>JsonBoolean</code> singleton. */
+	private static final JsonBoolean JSON_FALSE = new JsonBoolean(false);
+
+	/** Returns a pretty-printed <code>String</code> of this value with the given indent. */
+	public String toString(int indent) {
+		if (indent < 0) {
+			throw new IllegalArgumentException("Indent argument may not be negative.");
+		}
+
+		// if there is no indent, use the faster toString() method
+		if (indent == 0) {
+			return toString();
+		}
+
+		List<String> tokenList = new ArrayList<String>();
+		appendTokenList(tokenList);
+		int currentIndent = 0;
+		String currentIndentString = "";
+
+		StringBuilder sb = new StringBuilder();
+		final int len = tokenList.size();
+		for (int i = 0; i < len; ++i) {
+			String token = tokenList.get(i);
+			switch (token.charAt(0)) {
+				case '[':
+				case '{': {
+					sb.append(token);
+					currentIndent += indent;
+					currentIndentString = spaces(currentIndent);
+					break;
+				}
+				case ']':
+				case '}': {
+					currentIndent -= indent;
+					currentIndentString = spaces(currentIndent);
+					if (!(tokenList.get(i-1).equals("{") || tokenList.get(i-1).equals("[")))
+						sb.append(currentIndentString);
+					sb.append(token);
+					break;
+				}
+				case ':':
+					sb.append(": ");
+					break;
+				case '\t':
+					sb.append(currentIndentString);
+					break;
+				default:
+					sb.append(token);
+					break;
+			}
+		}
+
+		return sb.toString();
+	}
+
+	/** Appends all string tokens for this object to the given list. */
+	protected abstract void appendTokenList(List<String> tokenList);
+
+	/** Returns a <code>String</code> with the given number of spaces. */
+	private String spaces(int x) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < x; i++)
+			sb.append(" ");
+		return sb.toString();
+	}
+
+	/** Returns a static reference to JSON <code>null</code>. */
+	public static JsonNull getNull() {
+		return JSON_NULL;
+	}
+
+	/**
+	 * Returns a static reference to JSON <code>true</code> or <code>false</code>.
+	 */
+	public static JsonBoolean getBoolean(boolean b) {
+		return b ? JSON_TRUE : JSON_FALSE;
+	}
+
+	/** Returns a deep copy of this value. */
+	public JsonValue copy() {
+		return this;
+	}
+
+	/**
+	 * This method is similar to <code>copy()</code> method but complies with the
+	 * <code>Cloneable</code> interface. A deep copy of this value is returned.
+	 */
+	@Override
+	public Object clone() {
+		return copy();
+	}
+
+	/** Returns the JSON type of this value. */
+	public abstract Type getType();
+
+	/** Returns this value as a {@link JsonObject}. */
+	public JsonObject asMap() throws JsonCastException {
+		throw new JsonCastException(String.format("JsonValue %s cannot be cast to a JsonObject.",
+				toString()));
+	}
+
+	/** Returns this value as a {@link JsonArray}. */
+	public JsonArray asArray() throws JsonCastException {
+		throw new JsonCastException(String.format("JsonValue %s cannot be cast to a JsonArray.",
+				toString()));
+	}
+
+	/** Returns this value as a {@link Number}. */
+	public Number asNumber() throws JsonCastException {
+		throw new JsonCastException(String.format("JsonValue %s cannot be cast to a Number.",
+				toString()));
+	}
+
+	/** Returns this value as a {@link String}. */
+	public String asString() throws JsonCastException {
+		throw new JsonCastException(String.format("JsonValue %s cannot be cast to a string.",
+				toString()));
+	}
+
+	/** Returns this value as a <code>boolean</code>. */
+	public boolean asBoolean() throws JsonCastException {
+		throw new JsonCastException(String.format("JsonValue %s cannot be cast to a boolean.",
+				toString()));
+	}
+
+	/** Returns this value as a <code>Date</code>. */
+	public Date asDate() throws JsonCastException {
+		throw new JsonCastException(String.format("JsonValue %s cannot be cast to a date.",
+				toString()));
+	}
+
+	/**
+	 * A convenience method to Save this value to file with the given indent, assuming a UTF-8
+	 * character set.
+	 * 
+	 * @throws IOException
+	 *             If something goes wrong!
+	 */
+	public void saveToFile(File file, int indent) throws IOException {
+		saveToFile(file, indent, "UTF-8");
+	}
+
+	/**
+	 * A convenience method to save this value to file with the given indent and the character set
+	 * name.
+	 * 
+	 * @throws IOException
+	 *             If something goes wrong!
+	 */
+	public void saveToFile(File file, int indent, String charsetName) throws IOException {
+		BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file));
+		String jsonString = this.toString(indent);
+		byte[] data = jsonString.getBytes(charsetName);
+		out.write(data);
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * A convenience method to load a value from file assuming a UTF-8 character set.
+	 * 
+	 * @throws FileNotFoundException
+	 *             If the file could not be found.
+	 * @throws IOException
+	 *             If the file could not be read.
+	 * @throws JsonParseException
+	 *             If the file could not be parsed into valid JSON.
+	 */
+	public static JsonValue loadFromFile(File file) throws IOException {
+		return loadFromFile(file, "UTF-8");
+	}
+
+	/**
+	 * A convenience method to load a value from file with the given character set name.
+	 * 
+	 * @throws FileNotFoundException
+	 *             If the file could not be found.
+	 * @throws IOException
+	 *             If the file could not be read.
+	 * @throws JsonParseException
+	 *             If the file could not be parsed into valid JSON.
+	 */
+	public static JsonValue loadFromFile(File file, String charsetName)
+			throws FileNotFoundException, IOException, JsonParseException {
+		return loadFromStream(new BufferedInputStream(new FileInputStream(file)), charsetName);
+	}
+
+	/**
+	 * A convenience method to load a value from an input stream assuming a UTF-8 character set.
+	 * 
+	 * @throws IOException
+	 *             If the input stream could not be read.
+	 * @throws JsonParseException
+	 *             If the file could not be parsed into valid JSON.
+	 */
+	public static JsonValue loadFromStream(InputStream in) throws IOException, JsonParseException {
+		return loadFromStream(in, "UTF-8");
+	}
+
+	/**
+	 * A convenience method to load a value from an input stream with the given character set name.
+	 * 
+	 * @throws IOException
+	 *             If the input stream could not be read.
+	 * @throws JsonParseException
+	 *             If the file could not be parsed into valid JSON.
+	 */
+	public static JsonValue loadFromStream(InputStream in, String charsetName) throws IOException,
+			JsonParseException {
+		final ByteArrayOutputStream baos = new ByteArrayOutputStream(in.available());
+		byte[] buffer = new byte[1024]; // a 1KB buffer
+
+		int len = in.read(buffer);
+		while (len >= 0) {
+			baos.write(buffer, 0, len);
+			len = in.read(buffer);
+		}
+
+		return JsonObject.parse(new String(baos.toByteArray(), charsetName));
+	}
+
 }
